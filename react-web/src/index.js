f65f01bc2d03e12bde0d2bd7d9d0e3feac4a2e6f
Somkid <klovers.asia@gmail.com>
2020-03-21 12:38:16 +0700

    install npm redux-logger

--- react-web/src/index.js
+++ react-web/src/index.js
@@ -3,18 +3,25 @@ import ReactDOM from 'react-dom';
 
 import { BrowserRouter } from 'react-router-dom';
 
-import { createStore } from 'redux'
+import {applyMiddleware, createStore } from 'redux'
 import { Provider } from 'react-redux'
 
 import rootReducer from './reducers'
+
+// Logger with default options
+import logger from 'redux-logger'
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(logger));
 
-ReactDOM.render((<Provider store={store}><BrowserRouter><App /></BrowserRouter></Provider>), document.getElementById('root'));
+ReactDOM.render((<Provider store={store}>
+                    <BrowserRouter>
+                        <App />
+                    </BrowserRouter>
+                </Provider>), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
